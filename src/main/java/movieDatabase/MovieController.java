2f0a8d04596514433854afcfbf940557e42474d4
joelmswearingen <60490297+joelmswearingen@users.noreply.github.com>
2021-04-28 19:04:34 -0500

    Update MovieController.java

--- src/main/java/movieDatabase/MovieController.java
+++ src/main/java/movieDatabase/MovieController.java
@@ -3,9 +3,11 @@ package movieDatabase;
 public class MovieController {
 
     MovieStore movieStore;
+    MovieSearch movieSearch;
 
-    MovieController(MovieStore store) {
+    MovieController(MovieStore store, MovieSearch search) {
         movieStore = store;
+        movieSearch = search;
 
 
     }
